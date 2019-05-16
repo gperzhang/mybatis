dfe0f3344f7000cc570c78a6e86905caa17eaf75
gperzhang <49604184+gperzhang@users.noreply.github.com>
2019-05-16 15:08:38 +0800

    Delete MebatisTest.java

--- src/main/java/MebatisTest.java
+++
@@ -1,9 +0,0 @@
-import com.gupaoedu.mebatis.*;
-
-public class MebatisTest {
-    public static void main(String[] args) {
-        GpSqlSession gpSqlSession = new GpSqlSession(new GpConfigruation(),new GpExecutor());
-        BlogMapper mapper = gpSqlSession.getMapper(BlogMapper.class);
-        mapper.selectBlogById(1);
-    }
-}
